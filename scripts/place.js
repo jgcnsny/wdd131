963ebba98c99e41a9745fd95e58ef29188ffe969
jgcnsny <cansanayj@byui.edu>
2024-11-24 00:18:31 +0800

    Country Page

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,30 @@ document.getElementById('currentYear').textContent = currentYear;
 let lastModified = document.lastModified;
 
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+
+
+const temperature = 27;
+const conditions = "Sunny";
+const wind = 10;
+
+const tempElement = document.getElementById("temperature");
+const conditionsElement = document.getElementById("conditions");
+const windElement = document.getElementById("wind");
+const windchillElement = document.getElementById("windchill");
+
+function calculateWindChill() {
+    return 35.74 + 0.6215 * temperature - 35.75 * wind**0.16 + 0.4275 * temperature * wind**0.16;
+  }
+  
+  if (temperature <= 10  && wind > 4.8) {
+    windChill = calculateWindChill().toFixed(1);
+  } else {
+    windChill = "N/A";
+  }
+
+tempElement.innerHTML = `<strong>Temperature:</strong> ${temperature}°C`;
+conditionsElement.innerHTML = `<strong>Condition:</strong> ${conditions}`;
+windElement.innerHTML = `<strong>Wind Speed:</strong> ${wind} km/h`;
+windchillElement.innerHTML = `<strong>Wind Chill:</strong> ${windChill}°C`;
+
