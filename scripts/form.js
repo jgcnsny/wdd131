36c4db9efb1990f9a901d022803419323d530941
jgcnsny <cansanayj@byui.edu>
2024-12-07 11:56:31 +0800

    form

---
+++ scripts/form.js
@@ -0,0 +1,10 @@
+const currentYear = new Date().getFullYear();
+
+// Populate the current year in the footer
+document.getElementById('currentYear').textContent = currentYear;
+
+//Last Modified
+let lastModified = document.lastModified;
+
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
