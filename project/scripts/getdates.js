b50a47906d36767c863cb21027398500b67e47c0
jgcnsny <cansanayj@byui.edu>
2024-12-09 18:21:14 +0800

    Porject

---
+++ project/scripts/getdates.js
@@ -0,0 +1,18 @@
+const currentYear = new Date().getFullYear();
+
+// Populate the current year in the footer
+document.getElementById('currentYear').textContent = currentYear;
+
+//Last Modified
+let lastModified = document.lastModified;
+
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+
+const hamButton = document.querySelector('#myButton');
+const navigation = document.querySelector('.menuLinks');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
